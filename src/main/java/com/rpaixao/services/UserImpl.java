36e936259e15cfcbd0e1cf6a32a983b2592221d3
Ricardo Paixao <ricardod@gmlsoftware.com>
2019-10-09 19:49:41 -0500

    minor code cleanup

--- src/main/java/com/rpaixao/services/UserImpl.java
+++ src/main/java/com/rpaixao/services/UserImpl.java
@@ -33,9 +33,7 @@ public class UserImpl implements UserService {
 	public List<User> importReqRes() {
 		List<User> importedUsers = new ArrayList<User>();		
 		RestTemplate restTemplate = new RestTemplate();
-		try {
-		//ReqResWrapper result = restTemplate.getForObject(uri, ReqResWrapper.class);
-		
+		try {	
             HttpHeaders headers = new HttpHeaders();
             headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
             headers.add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
--- src/main/java/com/rpaixao/services/UserService.java
+++ src/main/java/com/rpaixao/services/UserService.java
@@ -1,10 +1,7 @@
 package com.rpaixao.services;
 
-
 import java.util.List;
-
 import com.rpaixao.models.User;
-
 
 public interface UserService {
 
